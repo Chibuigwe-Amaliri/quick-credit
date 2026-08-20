1001dc8f2d85b0240d7c4c54c7f68e8ad54f342f
unknown <amalirichibuigwe123@gmail.com>
2026-08-20 12:48:11 +0300

    fix: improve email validation error

--- app.js
+++ app.js
@@ -17,12 +17,28 @@ const port = process.env.PORT || 8080;
 app.use(authRoutes);
 app.use(usersRoute);
 
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+
+    res.status(status).json({
+        meta: {
+            statusCode: status,
+            error: message
+        },
+        data: {
+            result: data || {}
+        }
+    });
+});
+
 mongoose.connect(mongoURI)
 .then(result => {
     console.log('Connected to MongoDB');
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+        app.listen(port);
 }).catch(err => {
-    console.log(err);
+    const error = new Error(err);
+    error.statusCode = 500;
+    next(err);
   });
